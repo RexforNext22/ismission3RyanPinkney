5134cd0b06c4f7e26d3451f2f986a3756238040b
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2022-01-12 13:18:48 -0700

    added by ryan

---
+++ mission2RyanPinkney/Models/GradeCal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace mission2RyanPinkney.Models
+{
+    public class GradeCal
+    {
+            [Required]
+            public int iPercent { get; set; }
+
+    }
+}
