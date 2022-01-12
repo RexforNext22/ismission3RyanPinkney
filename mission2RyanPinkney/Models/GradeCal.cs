c13f47be4ce7f25d59bea53a6949127068d87151
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2022-01-12 14:22:01 -0700

    added by ryan

--- mission2RyanPinkney/Models/GradeCal.cs
+++ mission2RyanPinkney/Models/GradeCal.cs
@@ -6,20 +6,32 @@ namespace mission2RyanPinkney.Models
 {
     public class GradeCal
     {
+
         [Required]
+        [Range(0, 100)]
         public int iAssignment { get; set; }
 
         [Required]
+        [Range(0, 100)]
         public int iGroupProject { get; set; }
 
         [Required]
+        [Range(0, 100)]
         public int iQuizzes { get; set; }
 
         [Required]
+        [Range(0, 100)]
         public int iExams { get; set; }
 
         [Required]
+        [Range(0, 100)]
         public int iINTEX { get; set; }
+
+        //GradeCal oGradeCal = new GradeCal();
+
+        //double fPercent = (iAssignment * .55) + (iGroupProject * .05) + (iQuizzes * .10)
+        //    + (iExams * .20) + (iINTEX * .10);
+
 
     }
 }
